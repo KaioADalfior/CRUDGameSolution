128a17985b721db7a9cc3fd64d5e494d3473ed18
kaio <kaio@DESKTOP-BDKICA5>
2026-03-18 15:41:15 -0300

    Confecção tabelas de atributos e aparencias.

---
+++ TablesDB/Atributos.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Atributos (
+	idAtributo INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Forca INT NOT NULL,
+	Destreza INT NOT NULL,
+	Sabedoria INT NOT NULL,
+	Constituicao INT NOT NULL,
+	Inteligencia INT NOT NULL,
+	Carisma INT NOT NULL
+);
