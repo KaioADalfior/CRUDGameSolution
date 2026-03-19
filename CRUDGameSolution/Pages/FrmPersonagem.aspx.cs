09a00d337a2d1c83934d914b1f2ce4f79f7508a3
kaio <kaio@DESKTOP-BDKICA5>
2026-03-19 09:23:28 -0300

    Cadastro de aparencias e atributos sem personagens

--- CRUDGameSolution/Pages/FrmPersonagem.aspx.cs
+++ CRUDGameSolution/Pages/FrmPersonagem.aspx.cs
@@ -44,28 +44,87 @@ namespace CRUDGameSolution.Pages
             ddlSubclasse.Items.Insert(0, "Selecione...");
         }
 
+        private void LimparCampos()
+        {
+
+        }
+
         protected void Cadastrar()
         {
-            string nome = txtNome.Text;
-            string nivel = txtNivel.Text;
-            string sexo = txtSexo.Text;
+         
+            //atributos
 
-            //refatorar os index para pegar as DDLs.
+            string forca = txtForca.Text;
+            string destreza = txtDestreza.Text;
+            string sabedoria = txtSabedoria.Text;
+            string constituicao = txtConstituicao.Text;
+            string inteligencia = txtInteligencia.Text;
+            string carisma = txtCarisma.Text;
 
-            if(nome != "" && nivel != "" && sexo != "")
+            if(forca != "" && destreza != "" && sabedoria != "" && 
+                constituicao != "" && inteligencia != "" && carisma != "")
             {
-                lblMensagem.Text = "Preencha todos os campos!";
-                lblMensagem.CssClass = "text-danger";
+                Atributo novoAtributo = new Atributo();
+
+                novoAtributo.Forca = Convert.ToInt32(forca);
+                novoAtributo.Destreza = Convert.ToInt32(destreza);
+                novoAtributo.Sabedoria = Convert.ToInt32(sabedoria);
+                novoAtributo.Constituicao = Convert.ToInt32(constituicao);
+                novoAtributo.Inteligencia = Convert.ToInt32(inteligencia);
+                novoAtributo.Carisma = Convert.ToInt32(carisma);
+
+                string mensagem =
+                    DAOs.AtributoDAO.CadastrarAtributo(novoAtributo);
+
+                
+                //aparencia
+                string peso = txtPeso.Text;
+                string altura = txtAltura.Text;
+                string corCabelo = txtCorCabelo.Text;
+                string estiloCabelo = txtEstiloCabelo.Text;
+                string corOlhos = txtCorOlho.Text;
+                string corPele = txtCorPele.Text;
+
+                if(peso != "" && altura != "" && corCabelo != "" &&
+                    estiloCabelo != "" && corOlhos != "" && corPele != "")
+                {
+                    Aparencia novaAparencia = new Aparencia();
+
+                    novaAparencia.Peso = Convert.ToDecimal(peso);
+                    novaAparencia.Altura = Convert.ToDecimal(altura);
+                    novaAparencia.CorCabelo = corCabelo;
+                    novaAparencia.EstiloCabelo = estiloCabelo;
+                    novaAparencia.CorOlho = corOlhos;
+                    novaAparencia.CorPele = corPele;
+
+                    mensagem = DAOs.AparenciaDAO.CadastrarAparencia(novaAparencia);
+
+                    //cadastrar personagem aqui
+
+
+                    
+                    //personagem
+                    string nome = txtNome.Text;
+                    string nivel = txtNivel.Text;
+                    string sexo = txtSexo.Text;
+
+                    //ddls de raca e subclasse
+                    int indexRaca = ddlRaca.SelectedIndex;
+                    int indexSubclasse = ddlSubclasse.SelectedIndex;
+
+
+                    //refatorar os index para pegar as DDLs.       
+                }
+
             }
 
 
-
-            
+              
         }
 
         protected void btnConfirmar_Click(object sender, EventArgs e)
         {
-
+            Cadastrar();
         }
     }
 }
